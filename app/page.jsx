d0653de8ce41d9a15d716a023d58c1d7f147202d
Your NameJanAlSaifi <jan.al.saifi@googlemail.com>
2025-02-28 17:48:30 +0100

    added option and function for counting

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,52 @@
 "use client";
 import { useState } from "react";
-import { Button } from "react";
 
 export default function Home() {
   const [buttonMode, setButtonMode] = useState("mesa");
+  const [countMesa, setCountMesa] = useState({
+    Hummus: 0,
+    Tabouleh: 0,
+    Labneh: 0,
+    WalnussPaprikaPaste: 0,
+    Kartoffeln: 0,
+    Auberginen: 0,
+    Blumenkohl: 0,
+    Schneidebohnen: 0,
+    RoteBeeteSalat: 0,
+    Olivensalat: 0,
+    Bohnen: 0,
+    Karotten: 0,
+    KichererbsenSalat: 0,
+    Saubohnen: 0,
+    Spinat: 0,
+    Okraschoten: 0,
+    Kohlrabi: 0,
+    Artischockensalat: 0,
+    Moussaka: 0,
+    "Kartoffeln frittiert": 0,
+    Auberginenmus: 0,
+    Linsensalat: 0,
+    Bulgur: 0,
+  });
+
+  const [selected, setSelected] = useState(null);
+
+  const AddMesa = (item) => {
+    setCountMesa((prevCounts) => ({
+      ...prevCounts,
+      [item]: prevCounts[item] + 1,
+    }));
+  };
+  const SubMesa = (item) => {
+    setCountMesa((prevCounts) => ({
+      ...prevCounts,
+      [item]: prevCounts[item] - 1,
+    }));
+  };
+
+  const handleSelect = (value) => {
+    setSelected(selected === value ? null : value);
+  };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -35,7 +78,59 @@ export default function Home() {
         </button>
       </div>
       <div className="flex flex-col">
-        {buttonMode === "mesa" && <></>}
+        {buttonMode === "mesa" && (
+          <>
+            <div>
+              <h1 className="text-6xl m-3 text-center">Mesa</h1>
+              <div className="flex flex-row gap-4 my-6">
+                <input
+                  type="checkbox"
+                  id="eightMesa"
+                  name="eightMesa"
+                  value="eightMesa"
+                  className="hidden"
+                />
+                <label
+                  htmlFor="eightMesa"
+                  className={`cursor-pointer text-lg px-6 py-3 border-2 rounded-lg ${
+                    selected === "eightMesa"
+                      ? "border-violet-500 bg-violet-500"
+                      : ""
+                  } transition duration-200`}
+                  onClick={() => handleSelect("eightMesa")}
+                >
+                  8 Vorspeisen
+                </label>
+                <input
+                  type="checkbox"
+                  id="tenMesa"
+                  name="tenMesa"
+                  value="tenMesa"
+                  className="hidden"
+                />
+                <label
+                  htmlFor="tenMesa"
+                  className={`cursor-pointer text-lg px-6 py-3 border-2 rounded-lg ${
+                    selected === "tenMesa"
+                      ? "border-violet-500 bg-violet-500"
+                      : ""
+                  } transition duration-200`}
+                  onClick={() => handleSelect("tenMesa")}
+                >
+                  10 Vorspeisen
+                </label>
+              </div>
+              {Object.keys(countMesa).map((item) => (
+                <div key={item} className="mb-4 text-xl">
+                  <button onClick={() => AddMesa(item)}>{item}:</button>
+
+                  <span className="pl-7">{countMesa[item]}</span>
+                  <button onClick={() => SubMesa(item)}>-</button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
         {buttonMode === "menü" && <></>}
         {buttonMode === "fingerfood" && <></>}
         {buttonMode === "optionen" && <></>}
